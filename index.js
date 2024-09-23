4398565c636e91f6f1684844b59a776565e61e28
Ashishreddych <ashishreddy2506@gmail.com>
2024-09-23 14:18:31 -0700

    index.js modified

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ const vendorRoutes = require('./routes/vendorRoutes');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
 const bodyParser = require('body-parser');
-const path = require('path')
+const path = require('path');
+const cors = require('cors')
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000 ;
 
 
 // Load environment variables
 dotenv.config();
+app.use(cors())
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("MONGODB connected successfully"))
     .catch((error) => console.log(error));
 // Simple route for testing
-app.use('/', (req, res) => {
-    res.send("Welcome");
-});
+// app.use('/', (req, res) => {
+//     res.json({message: "Welcome"});
+// });
 
 
 // Middleware
